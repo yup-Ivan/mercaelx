2c820afce526921092037d2365c2ac79770d3176
yup-Ivan <158217864+yup-Ivan@users.noreply.github.com>
2025-01-27 19:17:41 +0100

    Delete PROYECTO MERCAELX DISENY directory

--- PROYECTO MERCAELX DISENY/JS/loadFooter.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("TEMPLATES/footer.html")
-        .then(response => response.text())
-        .then(html => {
-            document.querySelector("footer").innerHTML = html;
-        })
-        .catch(error => console.error("Error al cargar el footer:", error));
-});
